512372a471afccc963f9fe795f8bd90b0982451a
leonordd03 <leonorddomingues3@gmail.com>
2024-11-15 14:18:46 +0000

    update responsivity

--- outros testes/nav_bar/script.js
+++ outros testes/nav_bar/script.js
@@ -1,27 +1,38 @@
-const openMenuIcon = document.getElementById("open-menu");
-const menuContainer = document.getElementById("menu-container");
-const closeMenuIcon = document.getElementById("close-menu");
+window.addEventListener("load", () => {
+  const openMenuIcon = document.getElementById("open-menu");
+  const menuContainer = document.getElementById("menu-container");
+  const closeMenuIcon = document.getElementById("close-menu");
+  //const focusableElements = menuContainer.querySelectorAll("button, [href], input, select, textarea, [tabindex]:not([tabindex='-1'])");
 
-function OpenMenu() {
-  this.setAttribute("aria-expanded", "true");
-  openMenuIcon.classList.add("hide");
-  menuContainer.classList.remove("hide");
-  closeMenuIcon.focus();
-  closeMenuIcon.setAttribute("aria-expanded", "true");
-}
+  function setFocusableElements(enable) {
+    focusableElements.forEach((el) => {
+      if (enable) {
+        el.removeAttribute("tabindex");
+      } else {
+        el.setAttribute("tabindex", "-1");
+      }
+    });
+  }
 
-function CloseMenu() {
-  this.setAttribute("aria-expanded", "false");
-  openMenuIcon.classList.remove("hide");
-  openMenuIcon.focus();
-  menuContainer.classList.add("hide");
-  menuContainer.setAttribute("aria-hidden", "true");
-  openMenuIcon.setAttribute("aria-expanded", "false");
-}
+  function OpenMenu() {
+    this.setAttribute("aria-expanded", "true");
+    menuContainer.classList.remove("hide");
+    menuContainer.setAttribute("aria-hidden", "false");
+    //setFocusableElements(true); // Ativa os elementos focáveis
+    closeMenuIcon.focus();
+  }
 
-function OpenAndCloseMenu() {
+  function CloseMenu() {
+    this.setAttribute("aria-expanded", "false");
+    menuContainer.classList.add("hide");
+    menuContainer.setAttribute("aria-hidden", "true");
+    //setFocusableElements(false); // Desativa os elementos focáveis
+    openMenuIcon.focus();
+  }
+
   openMenuIcon.addEventListener("click", OpenMenu);
   closeMenuIcon.addEventListener("click", CloseMenu);
-}
 
-OpenAndCloseMenu();
+  // Desativa elementos focáveis ao carregar a página
+  //setFocusableElements(true);
+});
